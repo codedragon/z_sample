e3b66a8ded97bee6d927762d8d7d2b331f1392f4
Maria McKinley <mariak@mariakathryn.net>
2013-07-15 22:52:36 -0700

    highlight choice when mouse click

--- zillow.js
+++ zillow.js
@@ -15,3 +15,20 @@ ieHover = function() {
 //only necessary for IE
 if (window.attachEvent) window.attachEvent("onload", ieHover);
 
+// function for IE to click for our dropdown menu
+ieActive = function() {
+    // Only want navigation lists to dropdown.
+    var dDown = document.getElementById("nav").getElementsByTagName("LI");
+    for (var i=0; i<dDown.length; i++) {
+	dDown[i].onmousedown=function() {
+	    this.className+=" ieactive";
+	}
+	dDown[i].onmouseup=function() {
+	    // get rid of class when done
+	    this.className=this.className.replace(new RegExp(" ieactive\\b"), "");
+	}
+    }
+} 
+//only necessary for IE
+if (window.attachEvent) window.attachEvent("onload", ieActive);
+
